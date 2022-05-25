ae962adac195819ec72611c7e50f3c94426b134b
swenbao <95946132+swenbao@users.noreply.github.com>
2022-05-25 17:13:25 +0800

    1st git test on intelliJ added a constructor in Super_Store

--- src/entity/Super_Store.java
+++ src/entity/Super_Store.java
@@ -5,8 +5,11 @@ public class Super_Store {
 	///////////////////// attribute //////////////////
 	String storeName;
 	int cost;
-	
+
 	///////////////////// 測試一下啦 /////////////////
+	public Super_Store(String storeName){
+		this.storeName = storeName;
+	}
 	
 	
 }
